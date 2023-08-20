b1a755b67c498db5f3ba764f6fe97fb9a2e51110
Kwon Sang <kwonsg514@gmail.com>
2023-08-21 04:15:47 +0900

    Feat : create board app mainpage

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 
 app_name = 'board'
 
 urlpatterns = [
-
+    path('', TemplateView.as_view(template_name='board/question_list.html'), name='index')
 ]
