6157c42e01272bbad9c503fa09e3a739b73362cb
Kwon Sang <kwonsg514@gmail.com>
2023-08-21 06:16:57 +0900

    Feat : login and signup

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import SignupForm
+
+
+class UserCreateView(CreateView):
+    form_class = SignupForm
+    template_name = 'accounts/signup_form.html'
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/board'
+LOGOUT_REDIRECT_URL = '/board'
